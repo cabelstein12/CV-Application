b405d1f47888b9611d7c8ef4637aafcd43cccb5e
xzovyatwork <jsteinca@jci.com>
2024-09-11 21:09:02 +0000

    update output from personal form section

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,28 +1,41 @@
-import "./Form.css"
+import "./Form.css";
 
-export default function Info(){
-    return(
-        <>
-            <form className="form-section" id="info-form-section">
-                <h2>Personal</h2>
+export default function Info({
+  personalInfo,
+  changeFirstName,
+  changeLastName,
+  changeEmail,
+  changePhone,
+}) {
+  return (
+    <>
+      <form className="form-section" id="info-form-section">
+        <h2>Personal</h2>
 
-                <label htmlFor="first-name"> First Name </label>
-                <input type="text" id="first-name"/>
+        <label htmlFor="first-name"> First Name </label>
+        <input
+          type="text"
+          id="first-name"
+          value={personalInfo.firstName}
+          onChange={changeFirstName}
+        />
 
-                <label htmlFor="last-name"> Last Name </label>
-                <input type="text" id="last-name" />
+        <label htmlFor="last-name"> Last Name </label>
+        <input
+          type="text"
+          id="last-name"
+          value={personalInfo.lastName}
+          onChange={changeLastName}
+        />
 
-                <label htmlFor="field-name"> Career Title </label>
-                <input type="text" id="field-name" />
+        <label htmlFor="email"> Email </label>
+        <input type="email" value={personalInfo.email} onChange={changeEmail} />
 
-                <label htmlFor="email"> Email </label>
-                <input type='email' />
+        <label htmlFor="phone"> Phone </label>
+        <input type="tel" value={personalInfo.phone} onChange={changePhone} />
 
-                <label htmlFor="phone"> Phone </label>
-                <input type="tel" />
-
-                {/* <button>Add Information</button> */}
-            </form>
-        </>
-    )
+        {/* <button>Add Information</button> */}
+      </form>
+    </>
+  );
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,14 +1,26 @@
-import "./Card.css"
-import Info from './Info.jsx';
-import Education from './Education.jsx';
-import Experience from './Experience.jsx';
+import "./Card.css";
+import Info from "./Info.jsx";
+import Education from "./Education.jsx";
+import Experience from "./Experience.jsx";
 
-export default function Input(){
-    return (
-        <div className="content">
-            <Info />
-            <Education />
-            <Experience />
-        </div>
-    )
+export default function Input({
+  changeFirstName,
+  changeLastName,
+  changeEmail,
+  changePhone,
+  personalInfo,
+}) {
+  return (
+    <div className="content">
+      <Info
+        changeFirstName={changeFirstName}
+        changeLastName={changeLastName}
+        changeEmail={changeEmail}
+        changePhone={changePhone}
+        personalInfo={personalInfo}
+      />
+      <Education />
+      <Experience />
+    </div>
+  );
 }
