573d00e353e89f5caead0da08efece6069ec4d2c
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-02-06 09:21:14 -0700

    start XP component

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,8 @@
 import "./Form.css";
 import "./Card.css"
 
-export default function Experience() {
+let nextXpId = 0;
+export default function Experience({title, xp, company, roles, dates, onAddXp, onChangeTitle, onChangeCompanyName, onChangeResponsibilities, onChangeDates}) {
   return (
     <>
       <form
@@ -9,23 +10,27 @@ export default function Experience() {
         className="form-section content"
         onSubmit={(e) => {
           e.preventDefault();
+          onAddXp([
+            ...xp,
+            {id: nextXpId, jobTitle: title, companyName: company, responsibilities: roles, from: dates.from, to: dates.to}
+          ])
         }}
       >
         <h2>Experience</h2>
 
         <label htmlFor="job-title"> Job Title </label>
-        <input type="text" />
+        <input type="text" value={title} onChange={onChangeTitle}/>
 
         <label htmlFor="job-company"> Company </label>
-        <input type="text" />
+        <input type="text" value={company} onChange={onChangeCompanyName}/>
 
         <label htmlFor="job-responsibilities"> Responsibilities </label>
-        <textarea type="text" rows="4" cols="30" />
+        <textarea type="text" rows="4" cols="30" value={roles} onChange={onChangeResponsibilities}/>
 
         <label htmlFor="job-start">Start Date</label>
-        <input type="date" />
+        <input type="date" value={dates.from} onChange={onChangeDates}/>
         <label htmlFor="job-end">End Date</label>
-        <input id="end-date-text" type="date" />
+        <input id="end-date-text" type="date" value={dates.to} onChange={onChangeDates}/>
         <span> Present </span>
         <input
           id="end-date-checkbox"
